0f2bc27935d8db9340bf6e45ec5e91846826883e
sara2023s <a.dasilvaus@gmail.com>
2025-05-14 16:04:17 +1200

    user dahsboard ui optimizatipon

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -324,7 +324,7 @@ const UserDashboard = ({ activeTab }: UserDashboardProps) => {
 
         {/* Order Details Dialog */}
         <Dialog open={!!selectedOrder} onOpenChange={() => setSelectedOrder(null)}>
-          <DialogContent className="sm:max-w-[600px] [&>button]:hover:bg-primary [&>button]:hover:text-primary-foreground [&>button]:focus:ring-0 [&>button]:focus:ring-offset-0 [&>button]:hover:ring-0">
+          <DialogContent className="sm:max-w-[600px] [&>button]:hover:bg-primary [&>button]:hover:text-primary-foreground [&>button]:focus:ring-0 [&>button]:focus:ring-offset-0 [&>button]:hover:ring-0 focus:ring-0 focus:ring-offset-0 hover:ring-0">
             <DialogHeader>
               <DialogTitle>Detalhes do Pedido</DialogTitle>
               <DialogDescription>
